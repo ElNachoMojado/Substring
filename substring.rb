24a831eb99853f8ef66d19f5251daaffc02f8237
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-18 15:52:04 +0000

    Add comments

--- substring.rb
+++ substring.rb
@@ -4,14 +4,19 @@ def substrings (string, dictionary)
   #Arranges each word of string into an array and remove special characters if any
   string = string.split(' ').to_a.each {|word| word.downcase.gsub!(/[^0-9A-Za-z]/, '')}
   
+  #Search for each word in the string input
   string.each do |word|
+  #Search for each word/substring in the dictionary
     dictionary.each do |substring|
+  #Check if the word from the string contains the substring from dictionary
       if word.include?(substring)
+  #If true, push the substring into an array of matches
         matches.push(substring)
       end
     end
   end
 
+  #Return a hash from the matches array that hold the repetitions as values
   puts matches.tally
 
 end
