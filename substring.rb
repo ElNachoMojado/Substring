236d86af50190086943da6ee84a2c2572c43d243
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-18 15:49:36 +0000

    Add substring function that returns hash of matching substrings and the times they were repeated

--- substring.rb
+++ substring.rb
@@ -1,4 +1,24 @@
-def substrings ()
+def substrings (string, dictionary)
+  matches = Array.new
+
+  #Arranges each word of string into an array and remove special characters if any
+  string = string.split(' ').to_a.each {|word| word.downcase.gsub!(/[^0-9A-Za-z]/, '')}
+  
+  string.each do |word|
+    dictionary.each do |substring|
+      if word.include?(substring)
+        matches.push(substring)
+      end
+    end
+  end
+
+  puts matches.tally
+
 end
 
+puts "What would you like to check?"
+string = gets
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings(string, dictionary)
